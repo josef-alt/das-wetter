74b54661e3673552873e63d10e0269efad6ff211
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-05-16 09:31:57 -0500

    extracted loading current weather fields to function

--- app/src/main/java/com/example/daswetter/MainActivity.java
+++ app/src/main/java/com/example/daswetter/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.daswetter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView currentTempView, todaysHighView, todaysLowView;
     private TextView coverageView, currentFeelsLikeView, airQualityView;
+    private RelativeLayout day1, day2, day3, day4, day5, day6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +44,38 @@ public class MainActivity extends AppCompatActivity {
         coverageView = findViewById(R.id.cloudCoverage);
         currentFeelsLikeView = findViewById(R.id.feelsLike);
         airQualityView = findViewById(R.id.airQuality);
+
+        day1 = findViewById(R.id.day1);
+        TextView d1 = day1.findViewById(R.id.dayLabel);
+        Log.d("DAY1", d1.getText().toString());
+        day2 = findViewById(R.id.day2);
+        day3 = findViewById(R.id.day3);
+        day4 = findViewById(R.id.day4);
+        day5 = findViewById(R.id.day5);
+        day6 = findViewById(R.id.day6);
+
         location = preferences.getString("location", "Canada");
 
         weatherHandler = new Handler();
         Log.d("loc", location);
+        getSupportActionBar().setTitle(location);
         updateWeatherDisplay();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.settings) {
+            Toast.makeText(this, "Opened Settings Menu", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -70,20 +105,11 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<ForecastResponse> call, Response<ForecastResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     ForecastResponse forecastResponse = response.body();
-
                     Temperatures.Unit unit = Temperatures.Unit.valueOf(preferences.getString("temp_unit", "FAHRENHEIT"));
-                    double currentTemp = forecastResponse.getCurrent().getTemp(unit);
-                    double feelsLike = forecastResponse.getCurrent().getFeelsLike(unit);
-                    double high = forecastResponse.getForecast().getTodaysForecast().getDay().getMaxTemp(unit);
-                    double low = forecastResponse.getForecast().getTodaysForecast().getDay().getMinTemp(unit);
 
-                    currentTempView.setText(Temperatures.formatTemperature(currentTemp));
-                    todaysHighView.setText(Temperatures.formatTemperature(high));
-                    todaysLowView.setText(Temperatures.formatTemperature(low));
+                    setCurrentWeather(forecastResponse, unit);
 
-                    coverageView.setText(forecastResponse.getCurrent().getCondition().getText());
-                    currentFeelsLikeView.setText("RealFeel: " + Temperatures.formatTemperature(feelsLike));
-                    airQualityView.setText("Air Quality: " + forecastResponse.getCurrent().getAirQuality());
+                    Log.i("Forecast", forecastResponse.getForecast().getDailyForecast().size() + " days");
                 }
             }
 
@@ -92,5 +118,20 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+
+    private void setCurrentWeather(ForecastResponse forecastResponse, Temperatures.Unit unit) {
+        double currentTemp = forecastResponse.getCurrent().getTemp(unit);
+        double feelsLike = forecastResponse.getCurrent().getFeelsLike(unit);
+        double high = forecastResponse.getForecast().getTodaysForecast().getDay().getMaxTemp(unit);
+        double low = forecastResponse.getForecast().getTodaysForecast().getDay().getMinTemp(unit);
+
+        currentTempView.setText(Temperatures.formatTemperature(currentTemp));
+        todaysHighView.setText(Temperatures.formatTemperature(high));
+        todaysLowView.setText(Temperatures.formatTemperature(low));
+
+        coverageView.setText(forecastResponse.getCurrent().getCondition().getText());
+        currentFeelsLikeView.setText("RealFeel: " + Temperatures.formatTemperature(feelsLike));
+        airQualityView.setText("Air Quality: " + forecastResponse.getCurrent().getAirQuality());
     }
 }
