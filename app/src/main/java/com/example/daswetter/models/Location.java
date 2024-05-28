c4c2d78ebfb3a62327dd55ac05d8977beba62393
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-05-28 14:40:13 -0500

    added location attribute to current weather model

---
+++ app/src/main/java/com/example/daswetter/models/Location.java
@@ -0,0 +1,19 @@
+package com.example.daswetter.models;
+
+public class Location {
+    private String name;
+    private String region;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
